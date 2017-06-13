866516de88ba849d471510b6818be43d505c4896
rbas2015 <rbas2003@gmail.com>
2017-06-13 15:28:35 +0100

    Initial addition of delay model file

---
+++ journey/delay.jl
@@ -0,0 +1,11 @@
+##########################################################################################
+# Basic delay model
+#
+# Functions in this file implement the basic delay model of Nicolo and Silva (2017),
+# "Tomography of the London Underground: a Scalable Model for Origin-Destionation Data".
+#
+# NOTICE: THIS IS JUST THE BEGINNING OF A CONVERSION OF THE ORIGINAL MATLAB SCRIPTS!
+# NOT FINALIZED!
+
+
+
